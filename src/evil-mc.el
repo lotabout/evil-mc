7fcf6289d987e449913d3095b1e2b51d42196324
unknown <zhang_jin@worksap.jp.co>
2015-06-17 15:44:43 +0800

    temp update.

--- src/evil-mc.el
+++ src/evil-mc.el
@@ -20,6 +20,7 @@ saves the current state in the overlay to be restored later"
 
     ;; TODO: create a reagion overlay to simulate visual selection
 
+    (overlay-put overlay 'type 'fake-cursor)
     overlay))
 
 (defun evil-mc/remove-fake-cursor (o)
@@ -71,6 +72,7 @@ highlights the entire width of the window."
 				       evil-visual-point
 				       evil-visual-beginning
 				       evil-visual-end
+				       evil-this-register
 				       )
   "A list of vars that need to be tracked on a per-cursor basis.")
 
@@ -95,6 +97,90 @@ highlights the entire width of the window."
   (evil-mc/remove-fake-cursor o))
 
 ;;; ======================================================================
+;;; Cursor utilities for executing commands
+(defun evil-mc/fake-cursor-p (o)
+  "Predicate to check if an overlay is a fake cursor"
+  (eq (overlay-get o 'type) 'fake-cursor))
+
+(defun evil-mc/all-fake-cursors (&optional start end)
+  (remove-if-not 'evil-mc/fake-cursor-p
+                 (overlays-in (or start (point-min))
+                              (or end   (point-max)))))
+
+(defmacro evil-mc/for-each-fake-cursor (&rest forms)
+  "Runs the body for each fake cursor, bound to the name cursor"
+  `(mapc #'(lambda (cursor) ,@forms)
+         (evil-mc/all-fake-cursors)))
+
+(defmacro evil-mc/save-excursion (&rest forms)
+  "Saves and restores all the state that multiple-cursors cares about."
+  (let ((cs (make-symbol "current-state")))
+    `(let ((,cs (evil-mc/store-current-state-in-overlay
+                 (make-overlay (point) (point) nil nil t))))
+       (overlay-put ,cs 'type 'original-cursor)
+       (save-excursion ,@forms)
+       (evil-mc/pop-state-from-overlay ,cs))))
+
+(defmacro evil-mc/save-window-scroll (&rest forms)
+  "Saves and restores the window scroll position"
+  (let ((p (make-symbol "p"))
+        (s (make-symbol "start"))
+        (h (make-symbol "hscroll")))
+    `(let ((,p (set-marker (make-marker) (point)))
+           (,s (set-marker (make-marker) (window-start)))
+           (,h (window-hscroll)))
+       ,@forms
+       (goto-char ,p)
+       (set-window-start nil ,s t)
+       (set-window-hscroll nil ,h)
+       (set-marker ,p nil)
+       (set-marker ,s nil))))
+
+(defun evil-mc/execute-command-for-fake-cursor (cmd cursor)
+  (let ((evil-mc--executing-command-for-fake-cursor t)
+        (annoying-arrows-mode nil)
+        (smooth-scroll-margin 0))
+    (evil-mc/pop-state-from-overlay cursor)
+    (ignore-errors
+      (evil-mc/execute-command cmd)
+      (evil-mc/create-fake-cursor-at-point id))))
+
+(defun evil-mc/execute-command (cmd)
+  "Run command, simulating the parts of the command loop that makes sense for fake cursors."
+  (setq this-command cmd)
+  (run-hooks 'pre-command-hook)
+  (unless (eq this-command 'ignore)
+    (call-interactively cmd))
+  (run-hooks 'post-command-hook)
+  (when deactivate-mark (deactivate-mark)))
+
+(defun evil-mc/execute-command-for-all-fake-cursors (cmd)
+  "Calls CMD interactively for each cursor.
+It works by moving point to the fake cursor, setting
+up the proper environment, and then removing the cursor.
+After executing the command, it sets up a new fake
+cursor with updated info."
+  (evil-mc/save-excursion
+   (evil-mc/save-window-scroll
+    (evil-mc/for-each-fake-cursor
+     (save-excursion
+       (evil-mc/execute-command-for-fake-cursor cmd cursor))))))
+
+(defun evil-mc/execute-repeat-for-fake-cursor (repeat-info cursor)
+  (let ((evil-mc--executing-command-for-fake-cursor t)
+        (annoying-arrows-mode nil)
+        (smooth-scroll-margin 0))
+    (evil-mc/pop-state-from-overlay cursor)
+    (ignore-errors
+      (evil-execute-repeat-info repeat-info)
+      (evil-mc/create-fake-cursor-at-point id))))
+
+(defun evil-mc/execute-repeat-for-all-fake-cursors (repeat-info)
+  "execute the last opeartion by evil for all fake cursors"
+  (evil-mc/for-each-fake-cursor
+   (evil-mc/execute-repeat-for-fake-cursor repeat-info cursor)))
+
+;;; ======================================================================
 ;;; Evil part -- record every key sequence for every command.
 
 (defvar evil-last-keystrokes nil
@@ -115,21 +201,40 @@ highlights the entire width of the window."
   (evil-mc/record-keys (list "after" (this-command-keys))))
 
 (defun evil-mc/start-command-hook ()
+  (message (concat "start command: " (symbol-name this-command)))
+  (print evil-last-keystrokes)
   (evil-mc/start-record-keystrokes))
 
 (defun evil-mc/end-command-hook ()
+  (message (concat "stop command: " (symbol-name this-command)))
   (evil-mc/stop-record-keystrokes)
   (print evil-last-keystrokes)
   (setq evil-last-keystrokes nil))
 
-(add-hook 'pre-command-hook #'evil-mc/start-command-hook)
-(add-hook 'post-command-hook #'evil-mc/end-command-hook)
+;(add-hook 'pre-command-hook #'evil-mc/start-command-hook)
+;(add-hook 'post-command-hook #'evil-mc/end-command-hook)
 
-(defadvice read-key-sequence (before evil-mc activate)
-  "Record `this-command-keys' before it is reset."
-  (evil-mc/stop-record-keystrokes))
+;; (defadvice read-key-sequence (before evil-mc activate)
+;;   "Record `this-command-keys' before it is reset."
+;;   (message "read sequence")
+;;   (evil-mc/stop-record-keystrokes))
+
+;; (defadvice evil-operator-range (before evil-mc activate)
+;;   "Record `this-command-keys' before it is reset."
+;;   (message "operator-range"))
+
+;; (add-hook 'evil-repeat-post-hook-hook
+;; 	  '(lambda ()
+;; 	     (message (format "%s" (ring-ref evil-repeat-info 0)))))
 
 
+(defun evil-mc-post-repeat ()
+  (message "evil-repeat-post-hook: %s." (ring-ref evil-repeat-ring 0))
+  (evil-mc/execute-repeat-for-all-fake-cursors (ring-ref evil-repeat-ring 0)))
+
+;(add-function :after 'evil-repeat-post-hook #'evil-mc-post-repeat)
+(advice-add 'evil-repeat-post-hook :after #'evil-mc-post-repeat)
+(advice-remove 'evil-repeat-post-hook #'evil-mc-post-repeat)
 
 
 
